d1a85d99b15e4181f9b0c7eb9303a889dd0f06b5
aybike <tonn.raus09@gmail.com>
2021-04-14 17:25:13 +0200

    styles: playing with the globals

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,22 @@
 <script>
 	export let name;
 	export let lastName;
+	let svelteUrl = 'https://svelte.dev/tutorial'
 </script>
 
 <main>
 	<h1>Hello {name} {lastName}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Visit the <a href={svelteUrl}>Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
+	:global(:root){
+		--brand-color: #DD65DD;
+		--brand-color-bg: #1E2021;
+	}
+	:global(body){
+		background-color: var(--brand-color-bg);
+	}
 	main {
 		text-align: center;
 		padding: 1em;
@@ -17,10 +25,16 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: var(--brand-color);
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+	p{
+		color: #f2f2f2;
+	}
+	p a{
+		color: #66B2F0;
 	}
 
 	@media (min-width: 640px) {
